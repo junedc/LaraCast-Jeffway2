1cdf509d848623d74f9429b792c7981fccdc0a1f
LeonardoSC <leonardo@thesocialclub.co>
2018-09-10 22:39:11 +1200

    chapter 26 init

---
+++ chapter26/resources/js/bootstrap.js
@@ -0,0 +1,27 @@
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+
+window.Vue = Vue;
+Vue.use(VueRouter);
+
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
